567e4a865bbddefbd56904d2c3cae6d6d59258b9
Gustavo Nuñez <gustavonz99@gmail.com>
2023-06-22 12:49:35 -0700

    Proyecto inical del  django-blog

---
+++ blog/users/api/views.py
@@ -0,0 +1,19 @@
+#from users.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from users.api.serializers import UserRegisterSerializer
+
+class RegisterView(APIView):
+    def post(self,request):
+        # print('registrando usuario...')
+        serializer = UserRegisterSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+        
+
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
