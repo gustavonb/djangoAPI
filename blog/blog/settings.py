166370537291af8e375eec19ea67bcc8bbd03f7a
Gustavo Nuñez <gustavonz99@gmail.com>
2023-06-22 13:17:00 -0700

    se configuró JWT

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -115,6 +115,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':{
+        'rest_framework_sinplejwt.authentication.JWTAuthentication',
+    }
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
